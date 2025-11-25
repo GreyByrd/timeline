555ecbcab430451e8d296493c6b0f464c7921f11
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-11-25 07:00:37 +0000

    commit

--- src/components/decades/1970s/Event4.jsx
+++ src/components/decades/1970s/Event4.jsx
@@ -2,8 +2,8 @@ import "./style.css";
 
 const Event4 = () => {
   return (
-    <div className="event-container">
-      <div className="event-1-content">
+    <div className="event-container panel-gray">
+      <div className="event-4-content">
         <h1 className="date">July 9</h1>
         <h1 className="year">1972</h1>
         <h2>President Stark pins letter</h2>
@@ -12,16 +12,18 @@ const Event4 = () => {
           undertake a study of the single-sex nature of the College. Soon,
           serious discussions begin concerning next steps.
         </p>
+      </div>
+      <div className="event-4-content">
         <img
           className="event-1-image"
           src="src/assets/1970s_Collage_JackS.jpg"
           alt="Jack Stark."
         />
       </div>
-      <div className="event-content-wide">
+      <div className="event-4-content">
         <h1 className="date">November 17</h1>
         <h1 className="year">1972</h1>
-        <p className="blurb">
+        <p>
           Los Angeles Times article outlines potential shift to coeducation.
         </p>
       </div>
