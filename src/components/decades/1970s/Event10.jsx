f530777dac3ca69c27bd2458d5bec451bd4e576c
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-11-25 06:31:33 +0000

    commit

---
+++ src/components/decades/1970s/Event10.jsx
@@ -0,0 +1,24 @@
+import "./style.css";
+
+const Event10 = () => {
+  return (
+    <div className="event-container panel-purple">
+      <div className="event-1-content">
+        <h1 className="date white">February 27</h1>
+        <h1 className="year yellow">1971</h1>
+        <h2>Jil Stark becomes First Lady</h2>
+        <p>
+          Pretium nulla nisl at quis vel sed sit ut. Ornare eu eros consequat
+          proin amet. Feugiat ut sem id vitae sagittis.
+        </p>
+        <img
+          className="event-1-image"
+          src="src/assets/1970s_Collage_JS.jpg"
+          alt="Collage of Jil Stark on wedding day and by herself."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Event10;
