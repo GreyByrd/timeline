625d195bd57c25d759277f3cb2c641d8f2cfeebc
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-11-26 19:19:37 +0000

    Commit

--- src/components/decades/1970s/Event5.jsx
+++ src/components/decades/1970s/Event5.jsx
@@ -2,12 +2,14 @@ import "./style.css";
 
 const Event5 = () => {
   return (
-    <div className="event-container event-narrative">
-      <div className="event-narrative-content">
+    <div className="event-container event-narrative panel-yellow">
+      <div className="profile-content">
         <h1 className="year">1972</h1>
         <p>
           <em>Edessa Rose becomes first woman trustee.</em>
         </p>
+      </div>
+      <div className="profile">
         <img src="/src/assets/1970s_Edessa-Rose.png"></img>
       </div>
     </div>
