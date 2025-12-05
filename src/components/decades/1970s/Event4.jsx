569aed1aa0c04d07f8a807c907c80c3592fd1629
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-05 21:17:02 +0000

    Commit

--- src/components/decades/1970s/Event4.jsx
+++ src/components/decades/1970s/Event4.jsx
@@ -2,8 +2,8 @@ import "./style-1970.css";
 
 const Event4 = () => {
   return (
-    <div className="event-4-container panel-gray">
-      <div className="event-4-content">
+    <div className="event-container panel-gray">
+      <div className="col-1">
         <h1 className="date">July 9</h1>
         <h1 className="year">1972</h1>
         <h2>President Stark pins letter</h2>
@@ -13,14 +13,14 @@ const Event4 = () => {
           serious discussions begin concerning next steps.
         </p>
       </div>
-      <div className="event-4-content">
+      <div className="col-3">
         <img
           className="event-1-image"
           src="src/assets/1970s_Collage_JackS.jpg"
           alt="Jack Stark."
         />
       </div>
-      <div className="event-4-content">
+      <div className="col-1">
         <h1 className="date">November 17</h1>
         <h1 className="year">1972</h1>
         <p>
