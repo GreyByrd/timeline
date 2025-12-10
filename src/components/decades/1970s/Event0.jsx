d3e96994ff09b5c287818764800846c9cc0f953a
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-10 04:41:44 +0000

    Commit

--- src/components/decades/1970s/Event0.jsx
+++ src/components/decades/1970s/Event0.jsx
@@ -4,15 +4,28 @@ const Event0 = () => {
   return (
     <div className="event-container campaign">
       <div className="col-2">
-        <p className="caption">This is a caption.</p>
+        <p>
+          Sagittis diam massa vestibulum scelerisque felis bibendum ante. Sed
+          scelerisque dui quisque odio purus feugiat. Turpis viverra neque
+          turpis congue eu aliquet vulputate quis sagittis. Placerat commodo
+          commodo sed sed dignissim mattis ullamcorper. Aliquet platea euismod
+          et hac. Fermentum sollicitudin morbi tristique parturient orci ac
+          elementum.
+        </p>
       </div>
       <div className="col-2">
         <img className="profile-duo-01" src="/src/assets/stock02.jpg" />
         <p className="caption">This is a caption.</p>
       </div>
       <div className="col-2">
-        <img className="profile-duo-02" src="/src/assets/stock02.jpg" />
-        <p className="caption">This is a caption.</p>
+        <p>
+          Sagittis diam massa vestibulum scelerisque felis bibendum ante. Sed
+          scelerisque dui quisque odio purus feugiat. Turpis viverra neque
+          turpis congue eu aliquet vulputate quis sagittis. Placerat commodo
+          commodo sed sed dignissim mattis ullamcorper. Aliquet platea euismod
+          et hac. Fermentum sollicitudin morbi tristique parturient orci ac
+          elementum.
+        </p>
       </div>
     </div>
   );
---
+++ src/components/decades/1990s/Event1.jsx
@@ -0,0 +1,28 @@
+import "./style-1980.css";
+
+const Event1 = () => {
+  return (
+    <div className="event-container panel-white">
+      <div className="col-1">
+        <h1 className="maroon">1980s</h1>
+        <h2>The age of change</h2>
+        <p>
+          Sagittis diam massa vestibulum scelerisque felis bibendum ante. Sed
+          scelerisque dui quisque odio purus feugiat. Turpis viverra neque
+          turpis congue eu aliquet vulputate quis sagittis. Placerat commodo
+          commodo sed sed dignissim mattis ullamcorper. Aliquet platea euismod
+          et hac. Fermentum sollicitudin morbi tristique parturient orci ac
+          elementum.
+        </p>
+      </div>
+      <div className="intro-content">
+        <img
+          src="src/assets/1970s_1.gif"
+          alt="Collage of historical events in 1970s."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Event1;
