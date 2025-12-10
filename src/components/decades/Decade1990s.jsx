ae27310c3399e848a91f7f852036a94ffd37c3fd
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-10 05:02:21 +0000

    Commit

--- src/components/decades/Decade1990s.jsx
+++ src/components/decades/Decade1990s.jsx
@@ -7,12 +7,10 @@ import Event4 from "./1990s/Event4";
 const Decade1990s = () => {
   return (
     <ScrollyElement horizontal>
-      <div>
-        <Event1 id={1} />
-        <Event2 id={2} />
-        <Event3 id={3} />
-        <Event4 id={4} />
-      </div>
+      <Event1 id={1} />
+      <Event2 id={2} />
+      <Event3 id={3} />
+      <Event4 id={4} />
     </ScrollyElement>
   );
 };
