ea6bc3d2fc17fca91322a1d3715f49e109fb2812
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-12 17:44:47 +0000

    Commit

--- src/components/decades/Decade2000s.jsx
+++ src/components/decades/Decade2000s.jsx
@@ -4,11 +4,6 @@ import Event2 from "./2000s/Event2";
 import Event3 from "./2000s/Event3";
 import Event4 from "./2000s/Event4";
 import Event5 from "./2000s/Event5";
-import Event6 from "./2000s/Event6";
-import Event7 from "./2000s/Event7";
-import Event8 from "./2000s/Event8";
-import Event9 from "./2000s/Event9";
-import Event10 from "./2000s/Event10";
 
 const Decade2000s = () => {
   return (
@@ -18,11 +13,6 @@ const Decade2000s = () => {
       <Event3 id={3} />
       <Event4 id={4} />
       <Event5 id={5} />
-      <Event6 id={6} />
-      <Event7 id={7} />
-      <Event8 id={8} />
-      <Event9 id={9} />
-      <Event10 id={10} />
     </ScrollyElement>
   );
 };
