a2705053c93e084923156a13a9323f71e5f18c94
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-12 17:41:48 +0000

    Commit

--- src/components/decades/2000s/Event6.jsx
+++ src/components/decades/2000s/Event6.jsx
@@ -1,7 +1,27 @@
-import "./style-1990.css";
+import "./style-2000.css";
 
 const Event6 = () => {
-  return <div className="event-container-sm interstitial-01"></div>;
+  return (
+    <div className="event-container-sm panel-gray">
+      <div className="col-2">
+        <div>
+          <h1 className="date">April 8</h1>
+          <h1 className="year purple-txt">2006</h1>
+          <h2>Faculty Excellence</h2>
+          <p>
+            <strong>Professor Asuman Aksoy</strong> honored with the
+            Distinguished College or University Teaching Award from the Southern
+            California-Nevada Section of the Mathematical Association of
+            America. First woman professor at CMC to be selected, and second in
+            history.
+          </p>
+        </div>
+      </div>
+      <div className="col-3">
+        <img src="src/assets/placeholder.png" alt="Image." />
+      </div>
+    </div>
+  );
 };
 
 export default Event6;
