3a2d702ff4f6051fd0185beb73aa15a6707308c4
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-12 17:54:19 +0000

    Commit

--- src/components/decades/Decade2000s.jsx
+++ src/components/decades/Decade2000s.jsx
@@ -2,6 +2,13 @@ import ScrollyElement from "react-scrolly-telling/element";
 import Event1 from "./2000s/Event1";
 import Event2 from "./2000s/Event2";
 import Event3 from "./2000s/Event3";
+import Event4 from "./2000s/Event4";
+import Event5 from "./2000s/Event5";
+import Event6 from "./2000s/Event6";
+import Event7 from "./2000s/Event7";
+import Event8 from "./2000s/Event8";
+import Event9 from "./2000s/Event9";
+import Event10 from "./2000s/Event10";
 
 const Decade2000s = () => {
   return (
@@ -9,6 +16,13 @@ const Decade2000s = () => {
       <Event1 id={1} />
       <Event2 id={2} />
       <Event3 id={3} />
+      <Event4 id={4} />
+      <Event5 id={5} />
+      <Event6 id={6} />
+      <Event7 id={7} />
+      <Event8 id={8} />
+      <Event9 id={9} />
+      <Event10 id={10} />
     </ScrollyElement>
   );
 };
