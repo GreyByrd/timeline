dcdbea010aae9e023a327569942a3042ff6ac8ed
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-13 00:00:37 +0000

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Decade1970s from "./components/decades/Decade1970s";
 import Decade1980s from "./components/decades/Decade1980s";
 import Decade1990s from "./components/decades/Decade1990s";
 import Decade2000s from "./components/decades/Decade2000s";
+import Decade2010s from "./components/decades/Decade2010s";
 
 const App = () => {
   return (
