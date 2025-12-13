cb7c0348d25f9e4f935d174b309c08e168658b49
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-13 00:57:29 +0000

    Commit

---
+++ src/components/decades/2010s/Event4.jsx
@@ -0,0 +1,24 @@
+import "./style-2010.css";
+
+const Event4 = () => {
+  return (
+    <div className="event-container event-narrative panel-maroon">
+      <div className="col-1">
+      <h1 className="date">January 16</h1>
+        <h1 className="year">2011</h1>
+        <h2>Header</h2>
+        <p>
+          <strong>
+            March 2011: <a href="#">Center for Global Education</a>
+          </strong>
+          opens, encompassing CMC's Office of Off-Campus Study, including Study
+          Abroad and The Washington Program, as well as coordination and
+          promotion of fellowship programs and national awards for CMC students
+          and faculty.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Event4;
