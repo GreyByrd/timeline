04930c7af321860680ead71622ed482e68834819
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-15 17:40:47 +0000

    Commit

---
+++ src/components/decades/2010s/Event6.jsx
@@ -0,0 +1,25 @@
+import "./style-2010.css";
+
+const Event6 = () => {
+  return (
+    <div className="event-container-wide panel-white">
+      <div className="col-4">
+        <img src="/src/assets/fullwidth-placeholder.jpg" />
+      </div>
+      <div className="col-2">
+        <h1 className="date">September 13</h1>
+        <h1 className="year maroon">2014</h1>
+        <h2>
+          <a href="#">From the Mountain to the Sea</a> installed
+        </h2>
+        <p>
+          The artwork, the first public art installation by a woman,{" "}
+          <a href="#">Mary Weatherford</a> is acquired and featured in CMCs
+          signature program the Athenaeum.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Event6;
---
+++ src/components/decades/2010s/Event8.jsx
@@ -0,0 +1,17 @@
+import "./style-2010.css";
+
+const Event8 = () => {
+  return (
+    <div className="event-container event-narrative panel-white">
+      <h1 className="year maroon">2018</h1>
+      <p>
+        <strong>2017:</strong> First women CARE fellows
+      </p>
+      <p>
+        <strong>2018:</strong> ROTC Celebrates Centinnial
+      </p>
+    </div>
+  );
+};
+
+export default Event8;
