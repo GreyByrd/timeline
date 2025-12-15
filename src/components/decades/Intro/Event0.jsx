2ecb640f08218569270738d44c51112fd4fe3c1b
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-15 19:36:07 +0000

    Commit

--- src/components/decades/Intro/Event0.jsx
+++ src/components/decades/Intro/Event0.jsx
@@ -3,7 +3,7 @@ import "./style-intro.css";
 const Event0 = () => {
   return (
     <div className="img-container">
-      <img className="fullwidth" src="/src/assets/stock02.jpg" />
+      <img className="fullwidth" src="/src/assets/timeline-cover.png" />
     </div>
   );
 };
