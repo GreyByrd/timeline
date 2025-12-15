3997c6ac8150364ba912522a00925268a81bc050
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2025-12-15 06:27:36 +0000

    Commit

---
+++ src/components/decades/DecadeIntro.jsx
@@ -0,0 +1,12 @@
+import ScrollyElement from "react-scrolly-telling/element";
+import Event0 from "./Intro/Event0";
+
+const Decade1970s = () => {
+  return (
+    <ScrollyElement horizontal>
+      <Event0 id={0} />
+    </ScrollyElement>
+  );
+};
+
+export default DecadeIntro;
