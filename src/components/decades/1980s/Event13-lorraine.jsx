35b2fd7824269880431b01dbda3643aa17ab5574
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-01-29 20:18:21 +0000

    Commit

--- src/components/decades/1980s/Event13-lorraine.jsx
+++ src/components/decades/1980s/Event13-lorraine.jsx
@@ -3,25 +3,29 @@ import lorraine from "../../../assets/1980-profile-lorraine.png";
 
 const Event13 = () => {
   return (
-    <div className="event-container event-narrative panel-gray">
-      <img src={lorraine}></img>
-      <h1 className="year-sm maroon-txt">1988</h1>
-      <h2>Alumni Association breaks barriers</h2>
-      <p>
-        In 1988, <strong>Lorraine Bains ’88</strong> became the first woman and
-        first woman of color to serve as the CMC Alumni Association President. A
-        banking executive, she continues to serve CMC as a Trustee and a
-        co-chair of{" "}
-        <a
-          className="maroon-txt"
-          href="https://www.cmc.edu/50-coeducation"
-          target="_blank"
-          aria-label="CMC 50 years of Co-education (opens in new tab)"
-        >
-          CMC’s co-ed celebration
-        </a>
-        .
-      </p>
+    <div className="event-container-sm panel-gray">
+      <div className="col-3">
+        <img src={lorraine}></img>
+      </div>
+      <div className="col-3">
+        <h1 className="year-sm maroon-txt">1988</h1>
+        <h2>Alumni Association breaks barriers</h2>
+        <p>
+          In 1988, <strong>Lorraine Bains ’88</strong> became the first woman
+          and first woman of color to serve as the CMC Alumni Association
+          President. A banking executive, she continues to serve CMC as a
+          Trustee and a co-chair of{" "}
+          <a
+            className="maroon-txt"
+            href="https://www.cmc.edu/50-coeducation"
+            target="_blank"
+            aria-label="CMC 50 years of Co-education (opens in new tab)"
+          >
+            CMC’s co-ed celebration
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/decades/1980s/Event5-mari.jsx
+++ src/components/decades/1980s/Event5-mari.jsx
@@ -3,25 +3,29 @@ import mari from "../../../assets/1980-profile-mari.png";
 
 const Event5 = () => {
   return (
-    <div className="event-container event-narrative gradient-border-multi">
-      <img src={mari} />
-      <h1 className="year-sm white-txt">1980</h1>
-      <h2>Noteworthy Pioneer</h2>
-      <p>
-        <strong>Mari Adam ’80</strong> graduated magna cum laude with a double
-        major in French and Political Science as part of the College’s first
-        four-year co-ed class. An expert in financial planning and money
-        management, she serves as co-chair of{" "}
-        <a
-          className="white-txt"
-          href="https://www.cmc.edu/50-coeducation"
-          target="_blank"
-          aria-label="CMC 50 years of Co-education (opens in new tab)"
-        >
-          CMC’s co-ed celebration
-        </a>
-        .
-      </p>
+    <div className="event-container-sm gradient-border-multi">
+      <div className="col-3">
+        <img src={mari} />
+      </div>
+      <div className="col-3">
+        <h1 className="year-sm white-txt">1980</h1>
+        <h2>Noteworthy Pioneer</h2>
+        <p>
+          <strong>Mari Adam ’80</strong> graduated magna cum laude with a double
+          major in French and Political Science as part of the College’s first
+          four-year co-ed class. An expert in financial planning and money
+          management, she serves as co-chair of{" "}
+          <a
+            className="white-txt"
+            href="https://www.cmc.edu/50-coeducation"
+            target="_blank"
+            aria-label="CMC 50 years of Co-education (opens in new tab)"
+          >
+            CMC’s co-ed celebration
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 };
