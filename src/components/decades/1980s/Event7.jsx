8dcefd20a779cad973d91fbb4d0764571808e1df
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-01-29 00:31:43 +0000

    Commit

---
+++ src/components/decades/1980s/Event7.jsx
@@ -0,0 +1,14 @@
+import "./style-1980.css";
+
+const Event7 = () => {
+  return (
+    <div className="img-container panel-gray">
+      <img
+        className="fullwidth"
+        src="/src/assets/name-change-celebration-collage.png"
+      />
+    </div>
+  );
+};
+
+export default Event4;
