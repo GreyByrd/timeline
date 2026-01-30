991be888fc5cf13eed6fbe9b95a40ff53d912ab5
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-01-30 19:08:36 +0000

    Commit

--- src/components/decades/2000s/Event3-heather.jsx
+++ src/components/decades/2000s/Event3-heather.jsx
@@ -8,16 +8,10 @@ const Event3 = () => {
         <img src={heather} alt="Heather Antecol." />
       </div>
       <div className="col-3 white-txt">
-        <h1 className="date">October 24</h1>
         <h1 className="year-sm white-txt">2000</h1>
         <h2>Notable Dean</h2>
         <p>
-          In February 2021, <strong>Heather Antecol</strong>, the Boswell
-          Professor of Economics, was named Vice President and the first woman
-          to become Dean of the Faculty. Former director of the Berger
-          Institute, Antecol is a distinguished economics scholar whose
-          influential research on labor economics is recognized on a global
-          scale.
+          In February 2021, <strong>Heather Antecol</strong>, the Boswell Professor of Economics, was named Vice President and the first woman to become Dean of the Faculty.  Former director of the Berger Institute, Antecol is a distinguished economics scholar whose influential research on labor economics is recognized on a global scale.
         </p>
       </div>
     </div>
--- src/components/decades/Decade2000s.jsx
+++ src/components/decades/Decade2000s.jsx
@@ -1,14 +1,14 @@
 import ScrollyElement from "react-scrolly-telling/element";
 import Event1 from "./2000s/Event1";
-import Event2 from "./2000s/Event2";
-import Event3 from "./2000s/Event3";
-import Event4 from "./2000s/Event4";
-import Event5 from "./2000s/Event5";
-import Event6 from "./2000s/Event6";
-import Event7 from "./2000s/Event7";
-import Event8 from "./2000s/Event8";
-import Event9 from "./2000s/Event9";
-import Event10 from "./2000s/Event10";
+import Event2 from "./2000s/Event2-jeane";
+import Event3 from "./2000s/Event3-heather";
+import Event4 from "./2000s/Event4-suzi";
+import Event5 from "./2000s/Event5-dianeharpen";
+import Event6 from "./2000s/Event6-lisal";
+import Event7 from "./2000s/Event7-asuman";
+import Event8 from "./2000s/Event8-wangari";
+import Event9 from "./2000s/Event9-berger";
+import Event10 from "./2000s/Event10-fawe";
 import Event11 from "./2000s/Event11";
 
 const Decade2000s = () => {
