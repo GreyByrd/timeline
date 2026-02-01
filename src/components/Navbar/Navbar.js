01e7a8e3685f689031f7383086cd3fb698985a04
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-01 21:10:25 +0000

    Commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = React.forwardRef((props, ref) => {
+  const NavLinks = [
+    { name: "Home", link: "#top" },
+    { name: "About", link: "#About" },
+    { name: "Projects", link: "#Projects" },
+    { name: "Contact", link: "#Contact" },
+  ];
+
+  return (
+    <nav id="Navbar" className={styles.navbar} ref={ref}>
+      {NavLinks.map((navlink) => {
+        return (
+          <a key={navlink.name} href={navlink.link}>
+            {navlink.name}
+          </a>
+        );
+      })}
+    </nav>
+  );
+});
+
+export default Navbar;
