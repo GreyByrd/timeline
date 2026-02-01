c1457a40ad0917b901925bb07ca47d2bac5a0fa5
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-01 20:07:44 +0000

    Commit

--- src/components/decades/1990s/Event1.jsx
+++ src/components/decades/1990s/Event1.jsx
@@ -10,18 +10,18 @@ const Event1 = () => {
         <p>
           The fall of the Iron Curtain and the collapse of Communism showed that
           the 1990s would be a time of quickening change. The world watched as
-          Los Angeles erupted in race-related violence in 1992, a stark reminder
-          of the price of discrimination. The founding of the European Union
-          spoke of an increased focus on global interconnection and cooperation,
-          even as the first impeachment of a U.S. President in 130 years showed
-          how politics was still a full-contact sport. On the playing field, the
-          “Dream Team” U.S. men’s basketball team, the Women’s World Cup, and
-          the Summer Olympics in Atlanta highlighted the U.S.’s indomitable
-          position as a sports powerhouse, and yet the bombing at that same
-          Olympics and the shooting at Columbine High School showed tragedy
-          could come without notice. And in the world of technology, the arrival
-          of the first Web browser and the launch of Google and Amazon laid the
-          path for the “Information Superhighway.”
+          Los Angeles erupted in race-related violence in -Time1992, a stark
+          reminder of the price of discrimination. The founding of the European
+          Union spoke of an increased focus on global interconnection and
+          cooperation, even as the first impeachment of a U.S. President in 130
+          years showed how politics was still a full-contact sport. On the
+          playing field, the “Dream Team” U.S. men’s basketball team, the
+          Women’s World Cup, and the Summer Olympics in Atlanta highlighted the
+          U.S.’s indomitable position as a sports powerhouse, and yet the
+          bombing at that same Olympics and the shooting at Columbine High
+          School showed tragedy could come without notice. And in the world of
+          technology, the arrival of the first Web browser and the launch of
+          Google and Amazon laid the path for the “Information Superhighway.”
         </p>
       </div>
       <div className="col-3">
