a42cff1ac9ec1be1f5b61f1d95ad472be232782f
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-01 19:17:26 +0000

    Commit

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-export const Nav = styled.div`
-  display: flex;
-`;
-
-export const NavLinks = styled.ul`
-  list-style: none;
-  display: flex;
-
-  li {
-    margin: 0 10px;
-  }
+export const Wrapper = styled.div`
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 999;
 `;
 
 function Navigation() {
