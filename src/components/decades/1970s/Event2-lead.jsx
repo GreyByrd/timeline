bd32017573e8ff598516eb1898a536284e5ccaae
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-03 06:06:36 +0000

    Commit

--- src/components/decades/1970s/Event2-lead.jsx
+++ src/components/decades/1970s/Event2-lead.jsx
@@ -2,15 +2,18 @@ import "./style-1970.css";
 
 const Event2 = () => {
   return (
-    <div className="event-container event-narrative panel-white">
-      <div className="event-narrative-content">
+    <div className="container">
+      <div className="content">
         <p className="lead-text">
           Claremont Men’s College found itself at a crossroads, confronting a
-          consequential question:{" "}
-          <em>Who should we prepare for responsible leadership?</em> As with so
-          many other topics, the College approached the question thoughtfully
-          and thoroughly, and the earliest seeds of transformation would soon
-          take root.
+          consequential question: Jil Stark’s leadership, including six years
+          building the intellectual and social fabric of the College as Director
+          of the Athenaeum, impacted the campus and the community with enormous
+          generosity, tireless commitment, and gracious dedication while her
+          husband was President of the College from 1970 to 1999. She was also
+          an early advocate for co-education at CMC – when expanded
+          co-enrollment with Scripps College was put on the table, Jil worked
+          behind the scenes to steer CMC toward full enrollment for women.
         </p>
       </div>
     </div>
