6ef99a1d612b9efdd8e1faf1f4b8fcb3a6fc04c2
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-04 17:13:03 +0000

    Commit

--- src/components/decades/1970s/Event2-lead.jsx
+++ src/components/decades/1970s/Event2-lead.jsx
@@ -8,12 +8,7 @@ const Event2 = () => {
           Claremont Men’s College found itself at a crossroads, confronting a
           consequential question: Jil Stark’s leadership, including six years
           building the intellectual and social fabric of the College as Director
-          of the Athenaeum, impacted the campus and the community with enormous
-          generosity, tireless commitment, and gracious dedication while her
-          husband was President of the College from 1970 to 1999. She was also
-          an early advocate for co-education at CMC – when expanded
-          co-enrollment with Scripps College was put on the table, Jil worked
-          behind the scenes to steer CMC toward full enrollment for women.
+          of the Athenaeum, impacted the campus and the community.
         </p>
       </div>
     </div>
--- src/components/decades/1970s/Event4-JS.jsx
+++ src/components/decades/1970s/Event4-JS.jsx
@@ -4,39 +4,41 @@ import notformen from "../../../assets/not-for-men-only.png";
 
 const Event4 = () => {
   return (
-    <div className="event-container panel-gray">
-      <div className="col-2">
-        <h1 className="date">July</h1>
-        <h1 className="year purple-txt">1972</h1>
-        <h2>President Stark pens a letter</h2>
-        <p>
-          In a pivotal act, President Jack Stark urged the Claremont Men’s
-          College’s Board of Trustees to undertake a study of the single-sex
-          nature of the College: “I believe that the time is ripe to re-examine
-          the strengths and weaknesses of CMC as a single-sex college.” A
-          committee was formed and serious discussions began concerning next
-          steps.
-        </p>
-        <p>
-          Read President Stark's{" "}
-          <a
-            className="purple-txt"
-            href="https://cmcarchives.omeka.net/items/show/232"
-            target="_blank"
-            aria-label="CMC Archives (open in new tab)"
-          >
-            letter
-          </a>{" "}
-          to the Board.
-        </p>
-        <img
-          className="photo-insert-btn"
-          src={notformen}
-          alt="Button that says Not For Men Only."
-        />
-      </div>
-      <div className="col-3">
-        <img src={stark} alt="Jack Stark."></img>
+    <div className="container panel-gray">
+      <div className="content">
+        <div className="col-3">
+          <h1 className="date">July</h1>
+          <h1 className="year purple-txt">1972</h1>
+          <h2>President Stark pens a letter</h2>
+          <p>
+            In a pivotal act, President Jack Stark urged the Claremont Men’s
+            College’s Board of Trustees to undertake a study of the single-sex
+            nature of the College: “I believe that the time is ripe to
+            re-examine the strengths and weaknesses of CMC as a single-sex
+            college.” A committee was formed and serious discussions began
+            concerning next steps.
+          </p>
+          <p>
+            Read President Stark's{" "}
+            <a
+              className="purple-txt"
+              href="https://cmcarchives.omeka.net/items/show/232"
+              target="_blank"
+              aria-label="CMC Archives (open in new tab)"
+            >
+              letter
+            </a>{" "}
+            to the Board.
+          </p>
+          <img
+            className="photo-insert-btn"
+            src={notformen}
+            alt="Button that says Not For Men Only."
+          />
+        </div>
+        <div className="col-3">
+          <img src={stark} alt="Jack Stark."></img>
+        </div>
       </div>
     </div>
   );
