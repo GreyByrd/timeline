9c607711471e757170a7bda901e54767fb5cf5c8
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-06 01:03:50 +0000

    Commit

--- src/components/decades/2010s/Event7-mountain.jsx
+++ src/components/decades/2010s/Event7-mountain.jsx
@@ -4,7 +4,7 @@ import mountain from "../../../assets/2010-mountain-to-sea.jpg";
 const Event7 = () => {
   return (
     <div className="img-container">
-      <img className="red-outline" src={mountain} />
+      <img src={mountain} />
     </div>
   );
 };
