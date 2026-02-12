d053f9f6cf6a5fbb2a18d1c73c46e1efba4904a1
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-12 21:20:01 +0000

    Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import "./navstyle.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <nav className="decades">
-      <p>
-        <strong>Decades</strong>
-      </p>
-      <Link className="nav-links" to="1970" smooth={true} duration={200}>
-        1970
-      </Link>
-      <Link to="1980" smooth={true} duration={200} className="nav-links">
-        1980
-      </Link>
-      <Link to="1990" smooth={true} duration={200} className="nav-links">
-        1990
-      </Link>
-      <Link to="2000" smooth={true} duration={200} className="nav-links">
-        2000
-      </Link>
-      <Link to="2010" smooth={true} duration={200} className="nav-links">
-        2010
-      </Link>
-      <Link to="2020" smooth={true} duration={200} className="nav-links">
-        2020
-      </Link>
-    </nav>
+    <header>
+      <button className="nav-btn nav-open" onClick={toggleMenu}>
+        Decades
+      </button>
+
+      <nav className={isOpen ? "nav-menu active" : "nav-menu"}>
+        <Link to="1970" smooth={true} duration={200} onClick={closeMobileMenu}>
+          1970
+        </Link>
+        <Link to="1980" onClick={closeMobileMenu} smooth={true} duration={200}>
+          1980
+        </Link>
+        <Link to="1990" onClick={closeMobileMenu} smooth={true} duration={200}>
+          1990
+        </Link>
+        <Link to="2000" onClick={closeMobileMenu} smooth={true} duration={200}>
+          2000
+        </Link>
+        <Link to="2010" onClick={closeMobileMenu} smooth={true} duration={200}>
+          2010
+        </Link>
+        <Link to="2020" onClick={closeMobileMenu} smooth={true} duration={200}>
+          2020
+        </Link>
+      </nav>
+    </header>
   );
 };
 
