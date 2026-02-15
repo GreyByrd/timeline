c56c0c422081edfab841921ec6af2894761dd3a2
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-15 22:43:02 +0000

    Commit

--- src/components/decades/Intro/Event0.jsx
+++ src/components/decades/Intro/Event0.jsx
@@ -3,7 +3,7 @@ import "./style-intro.css";
 const Event0 = () => {
   return (
     <div className="event-container panel-gray">
-      <div className="content">
+      <div id="cover" className="content">
         <img className="fullwidth" src="/src/assets/timeline-cover.png" />
       </div>
     </div>
