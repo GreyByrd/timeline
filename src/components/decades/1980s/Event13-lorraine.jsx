769ec6528e564149719fbde58b3afeebf093849f
Isa Terrell <87440511+GreyByrd@users.noreply.github.com>
2026-02-26 18:57:10 +0000

    Commit

--- src/components/decades/1980s/Event13-lorraine.jsx
+++ src/components/decades/1980s/Event13-lorraine.jsx
@@ -10,10 +10,10 @@ const Event13 = () => {
           <h1 className="year-sm maroon-txt">1988</h1>
           <h2>Alumni Association breaks barriers</h2>
           <p>
-            In 1988, <strong>Lorraine Bains ’88</strong> became the first woman
-            and first woman of color to serve as the CMC Alumni Association
-            President. A banking executive, she continues to serve CMC as a
-            Trustee and a co-chair of{" "}
+            After graduating in 1988, <strong>Lorraine Bains</strong> went on to
+            become the first woman and first woman of color to serve as the CMC
+            Alumni Association President, taking on the role in 2003. A banking
+            executive, she continues to serve CMC as a Trustee and a co-chair of{" "}
             <a
               className="maroon-txt"
               href="https://www.cmc.edu/50-coeducation"
